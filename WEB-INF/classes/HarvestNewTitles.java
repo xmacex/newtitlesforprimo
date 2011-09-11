481e558e81107b9edc8c29db3c1f29f611b59f94
Mace Ojala <mace@kirjakaapeli.lib.hel.fi>
2011-09-11 23:56:33 +0300

    Real harvesting worked out in the class in drafts/ -folder, and moved to the servlet. Now let's test this...

--- WEB-INF/classes/HarvestNewTitles.java
+++ WEB-INF/classes/HarvestNewTitles.java
@@ -11,12 +11,14 @@ import org.apache.http.client.ClientProtocolException;
 import com.rdksys.oai.Harvester;
 import com.rdksys.oai.repository.Identify;
 import com.rdksys.oai.repository.Set;
+import com.rdksys.oai.data.Record;
+import com.rdksys.oai.data.RecordIterator;
 
 public class HarvestNewTitles extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 	throws IOException, ServletException {
 		// Let's digest some parameters
-		String paramvalue = request.getParameter("test");
+		String paramvalue = request.getParameter("set");
 		String repositoryurl = getServletContext().getInitParameter("repository.url");
 
 		// We need a writer, let's summon one
@@ -30,7 +32,6 @@ public class HarvestNewTitles extends HttpServlet {
 		out.println("kahtotaas josko löytyisi mitään haravoitavaa");
 
 		try {
-//			Harvester harvester = new Harvester("http://biblio.ugent.be/oai");
 			Harvester harvester = new Harvester(repositoryurl);
 			// Identify
 			Identify identify = harvester.identify();
@@ -38,10 +39,31 @@ public class HarvestNewTitles extends HttpServlet {
 			out.println(identify.getProtocolVersion());
 
 			// List sets
+			out.println("<h2>settejä</h2>");
+			out.println("<ul>");
 			List<Set> sets = harvester.listSets();
 			for(Set set : sets) {
+				out.println("<li>");
 				out.println(set.getSetName());
+				out.println("</li>");
    			}
+			out.println("</ul>");
+
+			// List titles in a set
+			String fromdate = "2011-06-01";
+			String untildate = "2011-06-30";
+			String set = paramvalue;
+
+			RecordIterator records = harvester.listRecords(fromdate, untildate, set);
+			out.println("<ol>");
+			while(records.hasNext()) {
+				Record record = records.next();
+				out.println("<li>");
+				System.out.println(record.getMetadata().getTitleList().get(0));
+				out.println("</li>");
+			}
+			out.println("</ol>");
+
 	        } catch (Exception ex) {
 		            ex.printStackTrace ();
 		}
--- drafts/HarvestNewTitles.java
+++ drafts/HarvestNewTitles.java
@@ -11,20 +11,34 @@ import org.apache.http.client.ClientProtocolException;
 import com.rdksys.oai.Harvester;
 import com.rdksys.oai.repository.Identify;
 import com.rdksys.oai.repository.Set;
+import com.rdksys.oai.data.Record;
+import com.rdksys.oai.data.RecordIterator;
 
 class HarvestNewTitles {
 	public static void main(String[] args) throws Exception {
-		Harvester harvester = new Harvester("http://biblio.ugent.be/oai");
-
+//		Harvester harvester = new Harvester("http://biblio.ugent.be/oai");
+		Harvester harvester = new Harvester("http://memory.loc.gov/cgi-bin/oai2_0");
 		// Identify
 		Identify identify = harvester.identify();
 		System.out.println(identify.getRepositoryName());
 		System.out.println(identify.getProtocolVersion());
 
 		// List sets
-		List<Set> sets = harvester.listSets();
-		for(Set set : sets) {
-			System.out.println(set.getSetName());
-   		}
+//		List<Set> sets = harvester.listSets();
+//		for(Set set : sets) {
+//			System.out.println(set.getSetName());
+//   		}
+
+		// List titles in a set
+		String fromdate = "2011-06-01";
+		String untildate = "2011-06-30";
+		String set = "";
+
+		RecordIterator records = harvester.listRecords(fromdate, untildate, set);
+		while(records.hasNext()) {
+			Record record = records.next();
+			System.out.println(record.getMetadata().getTitleList().get(0));
+		}
+
 	}
 }
